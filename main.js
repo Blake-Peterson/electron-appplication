77ef720e2ef156090d3bd1067fd1c10f8481aac0
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2025-02-26 05:58:40 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+/**
+ * app: application's event lifecycle
+ * BrowserWindow: creates and manages app windows
+ */
+const path = require('node:path')
+
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences:{
+        preload: path.join(__dirname,'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed',() => {
+    if(process.platform!=='darwin'){
+        app.quit();
+    }
+})
