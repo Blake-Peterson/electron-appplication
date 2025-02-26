e59c49ca001f6095d510b317f0dbabb1927201b2
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2025-02-26 06:14:38 -0600

    adjust ipcRender change

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 /**
  * app: application's event lifecycle
  * BrowserWindow: creates and manages app windows
@@ -7,19 +7,19 @@ const path = require('node:path')
 
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences:{
-        preload: path.join(__dirname,'preload.js')
-    }
-  })
-
-  win.loadFile('index.html')
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
+    })
+    win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-  createWindow()
+    ipcMain.handle('ping', () => 'pong')
+    createWindow()
 })
 
 app.on('window-all-closed',() => {
--- preload.js
+++ preload.js
@@ -1,8 +1,9 @@
-const {contextBridge, ipcRenderer} = require('electron');
+const { contextBridge, ipcRenderer } = require('electron')
 
-contextBridge.exposeInMainWorld('versions',{
-    node: () => process.versions.node,
-    chrome: ()=> process.versions.chrome,
-    electron: ()=> process.versions.electron,
-    ping: ()=> ipcRenderer.invoke('ping') //never expose ipcRenderer module directly
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke('ping') //Dont call ipcRenderer directly
+  // we can also expose variables, not just functions
 })
